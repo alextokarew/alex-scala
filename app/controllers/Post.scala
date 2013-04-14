cc56771a89aa93d1c3b1fd8a0854692804bc47e3
Alex tokarew <alex.tokarew@gmail.com>
2013-04-14 12:55:14 +0400

    Twitter bootstrap style

--- app/controllers/Post.scala
+++ app/controllers/Post.scala
@@ -3,12 +3,10 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-object Application extends Controller {
+object Post extends Controller {
   
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-
-  def contacts = TODO
   
 }
