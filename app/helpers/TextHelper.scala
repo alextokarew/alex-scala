b859411100107187bccc35d7585d0d5a2e48cdef
a.tokarev <a.tokarev@qiwi.ru>
2013-06-15 00:13:10 +0400

    Date formatting

--- app/helpers/TextHelper.scala
+++ app/helpers/TextHelper.scala
@@ -1,9 +1,18 @@
 package helpers
 
 import org.clapper.markwrap._
+import java.util.{Locale, Date}
+import java.text.{DateFormatSymbols, SimpleDateFormat}
 
 object TextHelper {
   val textileParser = MarkWrap.parserFor(MarkupType.Textile)
 
   def textile(text: String) : String = textileParser.parseToHTML(text)
+
+  def formatDate(date: Date) = new SimpleDateFormat("dd MMMMM yyyy, HH:mm", dateFormatSymbols).format(date)
+
+  val dateFormatSymbols = new DateFormatSymbols(new Locale("ru")) {
+    override def getMonths: Array[String] = Array("января", "февраля", "марта", "апреля", "мая", "июня", "июля",
+      "августа", "сентября", "октября", "ноября", "декабря")
+  }
 }
